b3443c8ad220e5659f0ae5685725a6f36cd82603
Ainsley Harewood <ainsley@deploy.global>
2018-01-23 12:29:43 -0400

    Minified HTML files

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,8 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     cleanCss = require('gulp-clean-css'),
     gulpIf = require('gulp-if'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    minifyHTML = require('gulp-minify-html');
 
 var env,
     coffeeSources,
@@ -76,7 +77,7 @@ gulp.task('watch', function() {
     gulp.watch(coffeeSources, ['coffee']);
     gulp.watch(jsSources, ['js']);
     gulp.watch('components/sass/*.scss', ['compass']);
-    gulp.watch(htmlSources, ['html']);
+    gulp.watch('builds/development/*.html', ['html']);
     gulp.watch(jsonSources, ['json']);
 });
 
@@ -88,7 +89,9 @@ gulp.task('connect', function() {
 });
 
 gulp.task('html', function() {
-    gulp.src(htmlSources)
+    gulp.src('builds/development/*.html')
+        .pipe(gulpIf(env === 'production', minifyHTML()))
+        .pipe(gulpIf(env === 'production', gulp.dest(outputDir)))
         .pipe(connect.reload())
 });
 
